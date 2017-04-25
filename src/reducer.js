22d1bb940387e952e8e0452e39fb71f1fad58f3c
Ben Costolo <ben.costolo@gmail.com>
2017-04-25 15:23:11 -0400

    fix: set initial state of reducer to empty object

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 import { actions } from './action';
 
-const reducer = (state, action) => {
+const reducer = (state = {}, action) => {
   switch (action.type) {
     case actions.TOGGLE_MENU:
       const { isOpen, menuId } = action.payload
